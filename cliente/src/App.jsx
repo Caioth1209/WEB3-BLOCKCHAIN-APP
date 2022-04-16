61e82a2a88a3a2907db905653ac77be39541d640
Caio Araujo <caiodaluz2004@gmail.com>
2022-04-15 21:04:07 -0300

    at

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,7 +1,6 @@
 import { Navbar, Footer, Loader, Services, Transactions, Welcome } from "./components"
 
 const App = ()=> {
-
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
